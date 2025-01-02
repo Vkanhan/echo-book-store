616d8874f21e77db05454e4a5ac4e484bcee0815
Vkanhan <kanhanvishnu@gmail.com>
2025-01-02 12:59:24 +0530

    refactored

--- json.go
+++ json.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 )
 
-func respondWithError(w http.ResponseWriter, code int, msg string) {
-	if code >= 500 {
+func respondWithError(w http.ResponseWriter, statusCode int, msg string) {
+	if statusCode >= 500 {
 		log.Printf("Responding with 5XX error: %s", msg)
 	}
 
@@ -15,18 +15,18 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 		Error string `json:"error"`
 	}
 
-	respondWithJSON(w, code, errResponse{Error: msg})
+	respondWithJSON(w, statusCode, errResponse{Error: msg})
 }
 
-func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+func respondWithJSON(w http.ResponseWriter, statusCode int, payload any) {
 	data, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Failed to marshal JSON response: %v", err)
-		w.WriteHeader(code)
+		w.WriteHeader(statusCode)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(code)
+	w.WriteHeader(statusCode)
 	w.Write(data)
 }
